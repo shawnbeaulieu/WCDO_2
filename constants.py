ce3d4d76f8f5b7484210b7ede75220e99a94291b
shawnbeaulieu <shawn.beaulieu@uvm.edu>
2025-07-08 13:01:53 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+cam_target_pos = [0, 0, 0] #  -0.2, 0.2]
+cam_distance = 48 # 2.05
+cam_yaw, cam_pitch, cam_roll = -50, -40, 0
+cam_width, cam_height = 480*2, 368*2
+cam_up, cam_up_axis_idx, cam_near_plane, cam_far_plane, cam_fov = [0, 0, 1], 2, 0.01, 100, 60
+
+cellRadius = 0.5
+
+petriDishWidth = 50
+
+motilityStrength = 5
+attractionStrength = 0.1
